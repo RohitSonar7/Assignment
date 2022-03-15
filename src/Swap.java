aee160644e37168c0bfc83e925ac89d365c1841a
unknown <sonarrohit7@gmail.com>
2022-03-15 22:18:09 +0530

    First Commit

---
+++ src/Swap.java
@@ -0,0 +1,13 @@
+public class Swap {
+    public static void main(String[] args) {
+
+        int x=2,y=6,t;
+        System.out.println("First number = " + x);
+        System.out.println("Second number = " + y);
+        t=x;
+        x=y;
+        y=t;
+        System.out.println("First number = " + x);
+        System.out.println("Second number = " +y);
+    }
+}
